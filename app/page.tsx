15c951024e513cb68f080efd0c6e614437b55c04
kriscard <contact@christophercardoso.dev>
2023-09-19 22:30:22 -0400

    Setup Prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
-export default function Home() {
+import prisma from "@/lib/db"
+
+export default async function Home() {
+  const users = await prisma.user.findMany()
+
+  console.log("users", users)
+
   return (
     <div className="flex items-center justify-center flex-col">
       <h1 className="font-heading text-2xl md:text-xl">Readme.ai</h1>
       <p className="font-sans text-lg md:text-sm">
         Create your Readme file in seconds
       </p>
+      <ul>
+        {users.map((user) => (
+          <li>{user.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
